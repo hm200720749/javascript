b316478e3bbbc356120d7a033a26378df5d9dccb
hm200720749 <200720749@qq.com>
2017-03-30 18:36:06 +0800

    upload

--- touchimg.js
+++ touchimg.js
@@ -2,6 +2,8 @@
  * Created by Administrator on 2017/3/29 0029.
  */
 function touchimg(options){
+    var j=0;
+    var timer;
     var elem=document.getElementById(options.elem);
     var temp_img=elem.innerHTML;
     //创建遮罩
@@ -11,11 +13,16 @@ function touchimg(options){
     wrap.innerHTML=temp_img;
     elem.innerHTML="";
     elem.appendChild(wrap);
-    //console.log(elem.innerHTML)
     var elem_ig_ul=elem.getElementsByClassName("img-ground")[0];
+
+    //var node_li=elem_ig_ul.getElementsByTagName("li")[0].cloneNode(true);
+    //elem_ig_ul.appendChild(node_li);
+    //console.log(elem_ig_ul);
     var elem_ig_li=elem_ig_ul.getElementsByTagName("li");
     if(elem=="undefined"||elem_ig_li=="undefined") return;
     var elem_width=elem.clientWidth;
+    //console.log(elem_width)
+    var img_width=elem_ig_li[0].getElementsByTagName('a')[0].getElementsByTagName("img")[0].clientWidth;
     var img_length=elem_ig_li.length;
     //ul 样式设置
     elem_ig_ul.style.width=elem_width*img_length+"px";
@@ -27,24 +34,82 @@ function touchimg(options){
     for(var i=0;i<img_length;i++){
         elem_ig_li[i].style.float="left";
         elem_ig_li[i].style.width=elem_width+"px";
+        elem_ig_li[i].getElementsByTagName('a')[0].getElementsByTagName("img")[0].style.width=elem_width>img_width?img_width:elem_width+"px";
     }
+    //滚动函数
+    function scroll_ul(fx){
+        if(fx=="left"){
+            if(j==0){
+                j=img_length-1;
+            }else{
+                j--;
+            }
+        }else if(fx=="right"){
+            if(j==img_length-1){
+                j=0;
+            }else{
+                j++;
+            }
+        }
+        elem_ig_ul.style.transform="translate(-"+elem_width*j+"px,0)";
+    }
+    function play(){
+        timer=setInterval(function(){scroll_ul("right")},1000);
+    }
+    function stop(){
+        clearInterval(timer);
+    }
+    elem.onmouseover=stop;
+    elem.onmouseout=play;
+    //play();
+    //touch
+    var startX,startY,j_change=0;
+    elem.addEventListener("touchstart",function(e){
+        elem_ig_ul.style.transition="all 0s";
+        var touch= e.touches[0];
+        startX=touch.clientX;
+        startY=touch.clientY;
+        stop();
+        e.preventDefault();
+    },false);
+    var maxscro=-elem_width*(img_length-1);
+    elem.addEventListener("touchmove",function(e){
+        var touch= e.changedTouches[0],
+            endX=touch.clientX,
+            endY=touch.clientY,
+            distanceX=endX-startX,
+            distanceY=endY-startY;
 
-   function loop_a(){
-       var j=0;
-       function loop_b(){
-           console.log(j);
-           if(j<img_length){
-               j++;
-           }else{
-               j=0;
-           }
-           //setTimeout(loop_b,1000);
-       }
-       //console.log(j)
-       return loop_b;
-   }
-    var loop=loop_a();
-    loop();
+            if(Math.abs(distanceX)>=Math.abs(distanceY)){
+                distanceX>0?j_change=-1:j_change=1;
+                var trans_x=-j*elem_width+distanceX;
+                console.log(trans_x);
+                console.log(maxscro);
+                if(trans_x>0||trans_x<maxscro){
+                    var scale_val=Math.abs(distanceX)*0.0015+1;
+                    var origin_tran="left center";
+                    var temp_x=0;
+                    trans_x<maxscro?origin_tran="right center":"";
+                    trans_x>0?temp_x=0:"";
+                    trans_x<maxscro?temp_x=maxscro:"";
+                    elem_ig_ul.style.transform="scale("+scale_val+") translate("+temp_x+"px,0)";
+                    elem_ig_ul.style.transformOrigin=origin_tran;
+                }else if(trans_x<0&&trans_x>maxscro){
+                    console.log(2);
+                    elem_ig_ul.style.transform="translate("+trans_x+"px,0)";
+                }
+                e.preventDefault();
+            }
 
-    //console.log(img_length)
+    },false);
+    elem.addEventListener("touchend",function(e){
+        elem_ig_ul.style.transition="all 0.5s";
+            if((j_change==-1&&j!=0)||(j_change==1&&j!=img_length-1)){
+                j+=j_change;
+            };
+            //elem_ig_ul.style.transform="translate(-"+elem_width*j+"px,0)";
+
+            //play();
+    },false);
+
 }
